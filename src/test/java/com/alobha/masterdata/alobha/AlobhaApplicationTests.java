8e02ea268993b942ffde93ab99fad9731e9017c3
Subha-Vaikundam <Subha.Rajeswaran@mnscorp.net>
2017-09-22 22:56:41 +0530

    New Repository

---
+++ src/test/java/com/alobha/masterdata/alobha/AlobhaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alobha.masterdata.alobha;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlobhaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
